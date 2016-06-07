a18ff968a230e578eeb3177c1924ff07c6e059d0
Dan Cropley <teracow@gmail.com>
2016-06-07 16:42:01 +1000

    Include a version of pstracker written to work with wget

---
+++ wget-launcher.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+# load several processes (up to a limit) then launch new ones as old ones exit.
+
+function InitPSTracker
+	{
+
+	child_count=0
+	child_limit=8
+	process_tracker_file="${temp_path}/child-process.count"
+
+	echo "${child_count}" > "${process_tracker_file}"
+
+	}
+
+function IncrementFile
+	{
+
+	# $1 = pathfile containing an integer to increment
+
+	if [ -z "$1" ] ; then
+		return 1
+	else
+		[ -e "$1" ] && count=$(<"$1") || count=0
+		((count++))
+		echo "$count" > "$1"
+	fi
+
+	}
+
+function DecrementFile
+	{
+
+	# $1 = pathfile containing an integer to decrement
+
+	if [ -z "$1" ] ; then
+		return 1
+	else
+		[ -e "$1" ] && count=$(<"$1") || count=0
+		((count--))
+		echo "$count" > "$1"
+	fi
+
+	}
+
+function SingleImageDownloader
+	{
+
+	# This function runs as a background process
+	# $1 = URL to download
+	# $2 = current counter relative to main list
+
+	IncrementFile "${process_tracker_file}"
+
+	echo "- starting download of link# [$2] ..."
+
+	# extract file extension
+	ext=$( echo $1 | sed "s/.*\(\.[^\.]*\)$/\1/" )
+
+	targetimage_pathfileext="${targetimage_pathfile}($2)${ext}"
+
+	wget --max-redirect 0 --timeout=20 --tries=3 --quiet --output-document "$targetimage_pathfileext" "$1"
+	result=$?
+
+	if [ $result -eq 0 ] ; then
+		echo "= finished download of link# [$2]: success!"
+		IncrementFile "${good_downloads_count_file}"
+	else
+		# increment failures_count but keep trying to download images
+		echo "= finished download of link# [$2]: failed!"
+		IncrementFile "${bad_downloads_count_file}"
+
+		# delete temp file if one was created
+		[ -e "${targetimage_pathfileext}" ] && rm -f "${targetimage_pathfileext}"
+	fi
+
+	DecrementFile "${process_tracker_file}"
+
+	}
+
+link_count=0
+max_images=26
+bad_count=0
+pids=""
+user_query="staci silverstone"
+image_file="google-image"
+current_path="$PWD"
+temp_path="/dev/shm"
+target_path="${current_path}/${user_query}"
+targetimage_pathfile="${target_path}/${image_file}"
+good_downloads_count_file="${temp_path}/successful-downloads.count"
+bad_downloads_count_file="${temp_path}/failed-downloads.count"
+countdown=$max_images		# this counter is used to control how many files are downloaded in total.
+
+[ -e "${good_downloads_count_file}" ] && rm -f "${good_downloads_count_file}"
+[ -e "${bad_downloads_count_file}" ] && rm -f "${bad_downloads_count_file}"
+
+InitPSTracker
+
+mkdir -p "${target_path}"
+
+while read msg || [[ -n "$msg" ]] ; do
+	((link_count++))
+
+	while true; do
+		child_count=$(<"${process_tracker_file}")
+
+		[ "$child_count" -lt "$child_limit" ] && break
+
+		sleep 0.5
+	done
+
+	if [ "$countdown" -gt 0 ] ; then
+		SingleImageDownloader "$msg" "$link_count" &
+		pids[${link_count}]=$!		# record PID for checking later
+		((countdown--))
+		sleep 0.1			# allow new child process time to spawn and update process counter file
+	else
+		# wait here while all current downloads finish
+		for pid in ${pids[*]}; do
+			wait $pid
+		done
+
+		# how many were successful?
+		[ -e "${good_downloads_count_file}" ] && good_count=$(<"${good_downloads_count_file}") || good_count=0
+
+		# not enough yet, so go get some more
+		if [ "$good_count" -lt "$max_images" ] ; then
+			# need to increase countdown again to get remaining files
+			countdown=$(($max_images-$good_count))
+		else
+			echo " *********** required number of files have been downloaded! ****************"
+			break
+		fi
+	fi
+
+done < "googliser-links.list"
+
+[ -e "${good_downloads_count_file}" ] && good_count=$(<"${good_downloads_count_file}") || good_count=0
+[ -e "${bad_downloads_count_file}" ] && bad_count=$(<"${bad_downloads_count_file}") || bad_count=0
+
+echo "all done!"
+echo "$good_count images were downloaded successfully and $bad_count failed."
